ddc70446a468e505d82cb57b1914896599aeb714
eazaykova <lena.zaykova.95@gmail.com>
2023-11-17 16:47:59 +0700

    Created RadioField

---
+++ src/components/ui/RadioField.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <input
+      type="radio"
+      :value="value"
+      :name="name"
+      :id="forID"
+      @change="onChange"
+      :checked="isChecked"
+    />
+    <label :for="forID">{{ title }}</label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "RadioField",
+  props: {
+    forID: String,
+    title: String,
+    value: String,
+    name: String,
+    isChecked: Boolean,
+  },
+  methods: {
+    onChange() {
+      this.$emit("getTime", event.target.value);
+    },
+  },
+};
+</script>
